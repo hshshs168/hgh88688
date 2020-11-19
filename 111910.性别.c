56b84ebcb2775bf367135658babe1334e5b967dd
hgh88688 <74358095+hgh88688@users.noreply.github.com>
2020-11-19 20:23:29 +0800

    Update 111910.性别.c

--- 111910.性别.c
+++ 111910.性别.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 void main(){
-	int i;
-	int a[4]={2020,2021,2022,2023};
-	for (i=0;i<=3;i++)
-	{
-	printf("%d\n",a[i]);
-	}
+int i;
+	scanf("%d",&i);
+if(i==1)
+{
+	printf("男");
+}
+else if(i==0)
+{
+printf("女");
+}
 }
