fdbfba37071e43a3e62b897386416d0368c40225
hgh88688 <74358095+hgh88688@users.noreply.github.com>
2020-11-25 09:59:21 +0800

    Update 112502.QS学校排名.app.c

--- 112502.QS学校排名.app.c
+++ 112502.QS学校排名.app.c
@@ -3,49 +3,51 @@ void main()
 {
 int x;
 scanf("%d",&x);
-if(x==1)
+if(x<=10)
 {
+if(x==1)
+    {
     printf("麻省理工学院");
-}
-  else  if(x==2)
+    }
+if(x==2)
     {
         printf("斯坦福大学");
     }
-     else    if(x==3)
+if(x==3)
     {
         printf("哈佛大学");
     }
-     else    if(x==4)
+if(x==4)
     {
         printf("牛津大学");
     }
-     else    if(x==5)
+if(x==5)
     {
         printf("加州理工学院");
     }
-     else    if(x==6)
+if(x==6)
     {
         printf("苏黎世联邦理工大学");
     }
-      else   if(x==7)
+ if(x==7)
     {
         printf("剑桥大学");
     }
-      else   if(x==8)
+if(x==8)
     {
         printf("伦敦大学学院");
     }
-      else   if(x==9)
+ if(x==9)
     {
         printf("帝国理工学院");
     }
-      else   if(x==10)
+if(x==10)
     {
         printf("芝加哥大学");
     }
+}
  else
     {
         printf("老铁的排名太靠后，请输入1-10");
     }
-
 }
