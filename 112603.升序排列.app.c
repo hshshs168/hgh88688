8dae6b7c232351874802f243a42227a000d85d0a
hgh88688 <74358095+hgh88688@users.noreply.github.com>
2020-11-26 21:24:49 +0800

    Update 112603.升序排列.app.c

--- 112603.升序排列.app.c
+++ 112603.升序排列.app.c
@@ -1,19 +1,24 @@
 #include<stdio.h>
 int main()
 {
-int a,b,c;
-printf("请输入3个整数，空格隔开，回车结束：");
+int a,b,c,t;
+printf("请输入3个数，空格隔开，回车结束\n");
 scanf("%d%d%d",&a,&b,&c);
-if(a>b&&a>c&&b>c)
-printf("%d\n%d\n%d\n",c,b,a);
-if(a>b&&a>c&&c>b)
-printf("%d\n%d\n%d\n",b,c,a);
-if(b>a&&b>c&&a>c)
-printf("%d\n%d\n%d\n",c,a,b);
-if(b>a&&b>c&&c>a)
-printf("%d\n%d\n%d\n",a,c,b);
-if(c>a&&c>b&&b>a)
+if(a>b)
+{t=a;
+a=b;
+b=t;
+}
+if(a>c)
+{t=a;
+a=c;
+c=t;
+}
+if(b>c)
+{
+t=b;
+b=c;
+c=t;
+}
 printf("%d\n%d\n%d\n",a,b,c);
-if(c>a&&c>b&&a>b)
-printf("%d\n%d\n%d\n",b,a,c);
 }
