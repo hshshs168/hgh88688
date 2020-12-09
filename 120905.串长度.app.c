e1907ae1ff39c637da8c4e131a6a2c3560d53097
hshshs168 <74294221+hshshs168@users.noreply.github.com>
2020-12-09 17:04:03 +0800

    Create 120905.串长度.app.c

---
+++ 120905.串长度.app.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char a1[]="power";
+    char a2[]="力量";
+
+    printf("%d",sizeof(a1));
+    printf("\n");
+    printf("%d",sizeof(a2));
+
+}
